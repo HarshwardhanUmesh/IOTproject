d9bf848bd2147a133ad1c0abdc1550a95a4501ab
HarshwardhanUmesh <139869924+HarshwardhanUmesh@users.noreply.github.com>
2023-09-13 16:20:43 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,7 +1,3 @@
-if (screen.width > 768) {
-    var mvp = document.getElementById('myViewport');
-    mvp.setAttribute('content','width=768');
-}
 $("p.btn.btn-primary.reset").click(function(){
   $.ajax({url: "https://potholedetection-f0b97ab1eedf.herokuapp.com/delete", success: function(result){
     alert("deleted");
