341cd8063994ea36cb0cf9a0494517ea275b9f63
HarshwardhanUmesh <139869924+HarshwardhanUmesh@users.noreply.github.com>
2023-09-13 11:33:57 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -11,3 +11,32 @@ $("p.btn.btn-primary.reset").click(function(){
   alert(err.Message);
 }});
 });
+
+
+$("#field-location svg.bi.bi-caret-down-fill").click(function (event) {
+    $("#field-location svg.bi.bi-caret-down-fill").css({visibility: "hidden"})
+    $("#field-location .card-body").animate({height: "50px"})
+    $("#field-location .card-body").children().animate({height: "0px"})
+    $("#field-location svg.bi.bi-caret-up-fill").css({visibility: "visible"})
+})
+
+$("#field-location svg.bi.bi-caret-up-fill").click(function (event) {
+    $("#field-location svg.bi.bi-caret-up-fill").css({visibility: "hidden"})
+    $("#field-location .card-body").animate({height: "342px"})
+    $("#field-location .card-body").children().animate({height: "330px"})
+    $("#field-location svg.bi.bi-caret-down-fill").css({visibility: "visible"})
+})
+
+$("#field-graph svg.bi.bi-caret-down-fill").click(function (event) {
+    $("#field-graph svg.bi.bi-caret-down-fill").css({visibility: "hidden"})
+    $("#field-graph .card-body").animate({height: "50px"})
+    $("#field-graph .card-body").children().animate({height: "0px"})
+    $("#field-graph svg.bi.bi-caret-up-fill").css({visibility: "visible"})
+})
+
+$("#field-graph svg.bi.bi-caret-up-fill").click(function (event) {
+    $("#field-graph svg.bi.bi-caret-up-fill").css({visibility: "hidden"})
+    $("#field-graph .card-body").animate({height: "342px"})
+    $("#field-graph .card-body").children().animate({height: "330px"})
+    $("#field-graph svg.bi.bi-caret-down-fill").css({visibility: "visible"})
+})
