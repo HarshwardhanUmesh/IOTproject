1413e26946306c2f3c67ff4fe79e0a1605e8ae25
HarshwardhanUmesh <139869924+HarshwardhanUmesh@users.noreply.github.com>
2024-05-28 17:03:34 +0530

    Update map.js

--- static/map.js
+++ static/map.js
@@ -14,7 +14,7 @@ var locatorIcon = L.icon({
 });
 
 var moderateIcon = L.icon({
-    iconUrl: 'https://i.imgur.com/Kz73Sjr.png',
+    iconUrl: 'https://i.imgur.com/MBvjFAD.png',
 
     iconSize:     [20, 20], // size of the icon
     iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
